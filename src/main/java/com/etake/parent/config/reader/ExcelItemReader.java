f7f32a9b67428881d4beec4c4d7a5e5748084b4c
Yaroslav <yarmoshyk96@gmail.com>
2023-05-08 11:08:18 +0300

    Change return type for ExcelItemReader

--- src/main/java/com/etake/parent/config/reader/ExcelItemReader.java
+++ src/main/java/com/etake/parent/config/reader/ExcelItemReader.java
@@ -5,13 +5,12 @@ import org.springframework.batch.extensions.excel.poi.PoiItemReader;
 import org.springframework.batch.extensions.excel.support.rowset.DefaultRowSetFactory;
 import org.springframework.batch.extensions.excel.support.rowset.RowNumberColumnNameExtractor;
 import org.springframework.batch.extensions.excel.support.rowset.RowSetFactory;
-import org.springframework.batch.item.ItemStreamReader;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExcelItemReader {
-    public <R> ItemStreamReader<R> getReader(final String filePath,
+    public <R> PoiItemReader<R> getReader(final String filePath,
                                                    final RowMapper<R> rowMapper,
                                                    final int skipLines,
                                                    final int headerRowIndex) {
